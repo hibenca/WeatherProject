bd2658b96c7ea7706f2092d2f037f951788879eb
hibenca <hibencorey@gmail.com>
2020-05-04 17:36:16 -0500

    Add a styles.css

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const bodyParser = require("body-Parser");
 const app = express();
 
 app.use(bodyParser.urlencoded( {extended: true} ));
+app.use(express.static(__dirname));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html")
+  res.sendFile(__dirname + "/" + "index.html")
   });
 
   app.post("/", function (req, res) {
